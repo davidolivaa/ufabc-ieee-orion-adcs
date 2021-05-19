9bd64a5f5621ed1c4ab9b8f2db928f0e393b05b8
David Oliveira Silva <davidoliva96@gmail.com>
2021-05-19 20:28:27 -0300

    initial commit

---
+++ octave_primeiros_passos/01-introducaoOctave.m
@@ -0,0 +1,28 @@
+alfa = 5*pi/9
+beta = pi/7
+
+res1 = cos(alfa) - cos(beta)
+res2 = 2*sin((alfa+beta)/2)*sin((beta-alfa)/2)
+
+
+x0 = 2;
+y0 = -3;
+
+A = 3;
+B = 5;
+C = -6;
+
+d = abs(A*x0+B*y0+C)/sqrt(A*2+B*2)
+
+
+preco_mesa = 256.95;
+preco_cadeira = 89.990;
+
+format bank
+
+preco_cadeira = 89.99;
+
+(2*preco_mesa+8*preco_cadeira)*1.055
+
+ans = round( (2*preco_mesa+8*preco_cadeira)*1.055 )
+
